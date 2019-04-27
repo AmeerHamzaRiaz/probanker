78f1998fd152c87c90a12c610ab616ae5c44e281
unknown <ameerhamza.rz@gmail.com>
2019-04-27 13:38:36 +0500

    does it work ?

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { createStackNavigator } from 'react-navigation';
 import SignUp from './src/components/SignUp';
 import Initial from './src/components/Initial';
+//import Initial from './src/components/Initial';
 
 
 export default class App extends Component {
